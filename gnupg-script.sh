f0e2ca6cae38c157393d7e4bda8a6319fdd1d5e0
Emilio Cuenca <106895403+cripsitorey@users.noreply.github.com>
2026-03-20 17:55:52 +0000

    Fourth Command

--- gnupg-script.sh
+++ gnupg-script.sh
@@ -1,5 +1,12 @@
+## 1
 uname -a #da informacion sobre el ordenador
 
+## 2
 which gpg #nos da la ubicacion del binario de gpg
 
+## 3
 gpg --version #nos dice la version de gpg e informacion adicional
+
+## 4
+gpg --full-generate-key
+
