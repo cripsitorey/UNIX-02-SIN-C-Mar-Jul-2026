bb3b0ce8451dbcf070c7a8dee085947984155969
Emilio Cuenca <106895403+cripsitorey@users.noreply.github.com>
2026-04-02 12:52:45 +0000

    wip: updating and installing packages

---
+++ fs_exploration.sh
@@ -0,0 +1,10 @@
+# 1 movernor al directorio raiz
+cd /
+# 2 listo enlaces simbolicos
+ls -F
+# 3 actualizo base de datos de repositorios
+sudo apt update
+# 4 actualizar
+sudo apt upgrade
+# 5 instalar paquete parted
+sudo apt install parted
