2a087787e987ae57cededc0f866686fdc353dcf0
Emilio Cuenca <106895403+cripsitorey@users.noreply.github.com>
2026-04-09 12:37:58 +0000

    wip: learning about cd and home dir

--- script_boot.sh
+++ script_boot.sh
@@ -3,3 +3,17 @@ cd proyeco/ #change direcorty to proyecto using relative paths
 cd /workspaces/UNIX-02-SIN-C-Mar-Jul-2026/proyecto # change directory to proyecto using absolute paths
 # we have relative and absolute paths, with absolute we have to put the entire path and this doesnt depend on the
 # current working directory and the relative depends on our working directory.
+
+pwd # print the working directory
+# ~ is the same as home dir
+# $HOME es enviromental variable of home dir
+cd ~
+cd $HOME
+cd 
+cd /home/codespace
+# all of them change dir to home dir
+
+$BASH
+$PATH 
+$HOME 
+#Example enviromental variable
