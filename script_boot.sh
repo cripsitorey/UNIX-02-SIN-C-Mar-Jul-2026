df78573bf2e7e009df50eff21e06a385ae8f5f71
Emilio Cuenca <106895403+cripsitorey@users.noreply.github.com>
2026-04-09 12:26:24 +0000

    wip: working with directories and paths

---
+++ script_boot.sh
@@ -0,0 +1,5 @@
+mkdir proyecto #make directory with name "Proyecto"
+cd proyeco/ #change direcorty to proyecto using relative paths
+cd /workspaces/UNIX-02-SIN-C-Mar-Jul-2026/proyecto # change directory to proyecto using absolute paths
+# we have relative and absolute paths, with absolute we have to put the entire path and this doesnt depend on the
+# current working directory and the relative depends on our working directory.
