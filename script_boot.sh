ce307979b8639886773c7fd8a68e5c85d191735f
Emilio Cuenca <106895403+cripsitorey@users.noreply.github.com>
2026-04-09 13:21:51 +0000

    wip: command marathon

--- script_boot.sh
+++ script_boot.sh
@@ -24,3 +24,32 @@ ls --all # the same as -a but bigger
 # . is here, .. is father dir
 man ls # show manual entry for ls
 
+ls -lai
+# total 8
+# 1573113 drwxrwxrwx+ 2 codespace codespace 4096 Apr  9 12:20 .
+# 1572874 drwxrwxrwx+ 5 codespace root      4096 Apr  9 12:20 ..
+# the number at the begginning is the inode, the letters after that are the permissions for owner, group and other
+# the following number is the amount of links which refer to that file, the next is the owner and the next the group,
+# the numbers "4096" the space using
+# the date and hour of creation
+
+stat .
+#   File: .
+#   Size: 4096            Blocks: 8          IO Block: 4096   directory
+# Device: 7,4     Inode: 1573113     Links: 2
+# Access: (0777/drwxrwxrwx)  Uid: ( 1000/codespace)   Gid: ( 1000/codespace)
+# Access: 2026-04-09 12:20:16.050307079 +0000
+# Modify: 2026-04-09 12:20:14.696307170 +0000
+# Change: 2026-04-09 12:20:14.696307170 +0000
+#  Birth: 2026-04-09 12:20:14.696307170 +0000
+
+# Device, primer numero disco si es 0 es virtual si no es fisico y el segundo la particion de ese disco
+
+
+pwd # print working directory
+whoami # usuario (quien soy)
+ls -lt # modificacion archivos 
+
+ls / # listo root
+ls /etc | head -20 # listo etc pero veo solo las primeras lineas
+ls /dev | tail -20 # listo dev pero veo solo las ultimas lineas
