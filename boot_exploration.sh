58ea95418e0905320f217e38d79c9a4b6ceec20d
Emilio Cuenca <106895403+cripsitorey@users.noreply.github.com>
2026-04-23 12:27:53 +0000

    redirecting stdout

--- boot_exploration.sh
+++ boot_exploration.sh
@@ -16,3 +16,4 @@ chmod o-r secreto.txt # Removing read to other
 chmod u+rw,go-rwx privado # Assing read and write to user and removing all to group and others
 
 echo "hola" | sudo tee /etc/archivo_protedigo > /dev/null # doing echo with "hola" content, and using tee to asing the output of echo to the file
+sudo sh -c 'echo "chao" >> /etc/archivo_protedigo' # Adding a new line in /etc/archivo_protedigo with the text "chao" redirecting echo stdout to the file
