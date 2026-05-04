0d0885a6725f0d11d5782570efb36db80d6a4fa6
Emilio Cuenca <106895403+cripsitorey@users.noreply.github.com>
2026-05-04 03:39:15 +0000

    Learning aptitude easter eggs

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -4,3 +4,8 @@ ls Documents # Lists the contents inside the 'Documents' directory
 ls -l # Lists files in long format showing permissions, owner, size, and date
 ls -l -r # Lists files in long format, sorted in reverse alphabetical order
 ls -rl # Same as above, combining both '-r' and '-l' flags into one
+
+aptitude moo # Displays a hidden easter egg message (requires aptitude installed)
+aptitude -v moo # Adds one level of verbosity to the easter egg
+aptitude -vv moo # Adds a second level of verbosity to the easter egg
+aptitude -vvv moo # Adds a third level of verbosity to the easter egg
