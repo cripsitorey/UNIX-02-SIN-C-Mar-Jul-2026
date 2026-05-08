53910906b0f0a4fc1266b9e74cbd9ed1bee02986
Emilio Cuenca <106895403+cripsitorey@users.noreply.github.com>
2026-05-08 16:17:50 +0000

    Cleared branch

--- calculadora/app.py
+++
@@ -1,5 +0,0 @@
-import suma
-Fsumando = input("ingresa el primer sumando ")
-Ssumando = input("ingresa el segundo sumando ")
-
-print(suma.sumar(int(Fsumando), int(Ssumando)))
--- saludo.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-int main(){
-    printf("Hola profe\n");
-    return 0;
-}
