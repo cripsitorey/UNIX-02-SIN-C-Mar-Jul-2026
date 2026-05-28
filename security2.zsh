e821036ba51971acc4f80be9b82d7ac15c7a1362
Emilio Cuenca <106895403+cripsitorey@users.noreply.github.com>
2026-05-28 12:58:34 +0000

    Groups part2

--- security2.zsh
+++ security2.zsh
@@ -2,3 +2,9 @@ id #See information about user
 id -gn #Display only the name of the main group
 touch ~/test_grupo_heredado.txt #Create a new file in the specified dir
 ls -la ~/test_grupo_heredado.txt #Check de group of the new file
+#Display current main group name
+id -gn 
+echo "Grupo actual: $(id-gn)"
+# Create a new file before  newgrp
+touch ~/antes_de_newgrp.txt
+ls ~/antes_de_newgrp.txt 
