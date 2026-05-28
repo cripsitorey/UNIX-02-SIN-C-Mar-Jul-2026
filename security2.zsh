553f6a9a65b1ef043e2a354100b4706b8e69579a
Emilio Cuenca <106895403+cripsitorey@users.noreply.github.com>
2026-05-28 12:49:55 +0000

    Learning about groups

---
+++ security2.zsh
@@ -0,0 +1,4 @@
+id #See information about user
+id -gn #Display only the name of the main group
+touch ~/test_grupo_heredado.txt #Create a new file in the specified dir
+ls -la ~/test_grupo_heredado.txt #Check de group of the new file
