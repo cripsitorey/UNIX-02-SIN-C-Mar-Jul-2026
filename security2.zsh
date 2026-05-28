13a84e221a37ef16f6fc68a148aaa4a59c31e756
Emilio Cuenca <106895403+cripsitorey@users.noreply.github.com>
2026-05-28 13:15:58 +0000

    Groups part 4 (files with newgrp)

--- security2.zsh
+++ security2.zsh
@@ -20,3 +20,9 @@ newgrp desarrolladores
 ## Checking if it worked displaying group name
 id -gn
 echo "Nuevo grupo activo: $(id-gn)"
+## Creating a file into the subshell
+touch ~/dentro_de_desarrolladores.txt
+ls -la ~/dentro_de_desarrolladores.txt
+## Creating a new dir
+mkdir -p ~/proyecto_dev/src
+ls -la ~/   
