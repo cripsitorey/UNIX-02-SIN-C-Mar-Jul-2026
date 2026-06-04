5fa764d4624252dc558fb12bbcdde3c1862efb27
Emilio Cuenca <106895403+cripsitorey@users.noreply.github.com>
2026-06-04 13:01:20 +0000

    wip

--- blackhatbash1.sh
+++ blackhatbash1.sh
@@ -1,6 +1,15 @@
+#!/bin/bash
 bash --version
+set -x
 env
+set +x
 echo ${SHELL}
 echo ${RANDOM}
 echo ${UID}
 echo ${OSTYPE}
+ps -ef
+df -h
+#man
+#bash -x blackhatbash1.sh as shell
+#bash -r blackhatbash1.sh 
+#bash -n blackhatbash1.sh debug
