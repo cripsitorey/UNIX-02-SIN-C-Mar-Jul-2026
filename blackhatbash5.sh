72408ca1a52daaeb328610872b5d8d151387fef5
Emilio Cuenca <106895403+cripsitorey@users.noreply.github.com>
2026-06-05 17:15:31 +0000

    Learning stdin out and err

--- blackhatbash5.sh
+++ blackhatbash5.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 top #Executes top tool in the foreground
 top & #Executes top tool in the background
+(ls; ps)
+ls; ps; whoami
+lzl || echo "el comando lzl fallo"
+echo "Hola mundo" > output1.txt
+cat output1.txt
+echo "Chao mundo" >> output1.txt
+cat output1.txt
+ls -l / &> stdout_and_stderr.txt
+cat stdout_andstderr.txt
+ls -l / &>> stdout_and_stderr.txt
