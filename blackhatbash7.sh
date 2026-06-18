cb4ea8f5254d20ca1a769b207652980254f870a2
Emilio Cuenca <106895403+cripsitorey@users.noreply.github.com>
2026-06-18 13:07:03 +0000

    working with data files

--- blackhatbash7.sh
+++ blackhatbash7.sh
@@ -10,3 +10,7 @@ awk '{print $1}' log.txt
 awk '{print $1,$2,$3}' log.txt
 awk '{print $2}' log.txt
 awk '{print $3}' log.txt
+awk '{print $1,$NF}' log.txt
+awk -F',' '{print $1}' example_csv.txt
+head log.txt
+awk 'NR < 10' log.txt
---
+++ popullate_csv.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+for i in {1..3}
+do
+  echo "Hola$i,Chao$i" >> example_csv.txt
+done
