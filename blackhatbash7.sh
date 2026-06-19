69f8213fdb7f0f530fb9699bda244b314aaa121a
Emilio Cuenca <106895403+cripsitorey@users.noreply.github.com>
2026-06-19 17:11:58 +0000

    using control z and suspend

--- blackhatbash7.sh
+++ blackhatbash7.sh
@@ -26,4 +26,9 @@ sed -i '$d' newlog.txt
 sed 's/ //g' newlog.txt > newlog1.txt
 grep " " newlog1.txt
 sed '8,10d' newlog1.txt
-sed -n '10,15 p' log.txt
+sed -n '10,15 p' newlog1.txt
+
+sleep 300 &
+jobs
+fg %1
+bg %1
