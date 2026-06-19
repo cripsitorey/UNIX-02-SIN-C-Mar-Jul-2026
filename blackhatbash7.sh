586d1bd22a4b82698650c641444e56b5b15003e8
Emilio Cuenca <106895403+cripsitorey@users.noreply.github.com>
2026-06-19 16:43:54 +0000

    learning sed

--- blackhatbash7.sh
+++ blackhatbash7.sh
@@ -15,3 +15,7 @@ awk -F',' '{print $1}' example_csv.txt
 head log.txt
 awk 'NR < 10' log.txt
 grep "42.236.10.117" log.txt | awk '{print $7}'
+sed 's/Mozilla/Godzilla/g' log.txt
+grep "zilla" log.txt
+sed 's/Mozilla/Godzilla/g' log.txt > newlog.txt
+grep "zilla" log.txt
